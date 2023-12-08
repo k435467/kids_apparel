1d5d50db0cc67999a4ad1a0f9124dec0184dfa7b
EthanGuo <k435467@gmail.com>
2023-12-09 00:09:28 +0800

    feat(auth): add prisma and next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import NextAuth, { type AuthOptions } from 'next-auth'
+import GithubProvider, { type GithubProfile } from 'next-auth/providers/github'
+
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+      profile(profile: GithubProfile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name,
+          email: profile.email,
+          image: profile.avatar_url,
+          // role: '',
+        }
+      },
+    }),
+  ],
+  // debug: true, // TODO - disable debug on production
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.role = user.role
+      }
+      return token
+    },
+    session({ session, token }) {
+      session.user = {
+        ...session.user,
+        role: token.role,
+      }
+      return session
+    },
+  },
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-      <div>Home</div>
-    </main>
-  )
+  return <div>Home</div>
 }
