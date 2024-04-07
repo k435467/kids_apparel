ce07aa3cc5087f5b1f204e53ece23ecae1fed6ea
EthanGuo <k435467@gmail.com>
2024-04-07 17:34:39 +0800

    feat: add logo

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 import { SidebarButton } from '@/components/layout/SidebarButton'
 import { Cart } from '@/components/layout/Cart'
+import Image from 'next/image'
+import BubbleBubbleLogo from '@/assets/bubble-bubble-logo.png'
 
 export const Navbar: React.FC<{}> = () => {
   return (
@@ -10,7 +12,9 @@ export const Navbar: React.FC<{}> = () => {
         <SidebarButton />
       </div>
       <div className="flex items-center justify-center font-thin">
-        <Link href="/">LOGO</Link>
+        <Link href="/">
+          <Image width={130} height={48} src={BubbleBubbleLogo} alt="logo" />
+        </Link>
       </div>
       <div className="mr-4 flex items-center justify-end">
         <Cart />
