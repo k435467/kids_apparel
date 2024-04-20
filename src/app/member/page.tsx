fe59cd68adf7b701ea1f4480f0c750440144d35d
EthanGuo <k435467@gmail.com>
2024-04-20 16:04:35 +0800

    feat(member): redesign layout

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -1,15 +1,13 @@
 'use client'
-import { UserOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
-import { signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
 import React from 'react'
+import { Descriptions } from 'antd'
+import type { DescriptionsProps } from 'antd'
 
 /**
  * 使用者資料頁面
  */
 export default function () {
-  const router = useRouter()
   const { data: session } = useSession()
 
   const user = session?.user
@@ -17,29 +15,36 @@ export default function () {
     return <div>請先登入</div>
   }
 
-  return (
-    <div>
-      <div className="mt-8 flex flex-col items-center">
-        {user.image ? (
-          <img className="h-40 w-40 rounded-full" src={user.image} alt="" />
-        ) : (
-          <UserOutlined className="text-9xl" />
-        )}
+  const items: DescriptionsProps['items'] = [
+    {
+      key: 'userName',
+      label: '名稱',
+      children: user.userName,
+    },
+    {
+      key: 'phoneNumber',
+      label: '手機',
+      children: user.phoneNumber,
+    },
+    ...(user.role
+      ? [
+          {
+            key: 'role',
+            label: '角色',
+            children: user.role,
+          },
+        ]
+      : []),
+  ]
 
-        <div className="mt-4 text-3xl">{user.userName}</div>
-        <div className="mt-4 text-xl">{user.phoneNumber}</div>
-        {user.role && <div className="mt-4 text-xl">{user.role.toUpperCase()}</div>}
-        <Button
-          className="mt-4"
-          danger
-          onClick={() => {
-            signOut({ redirect: false })
-            router.replace('/')
-          }}
-        >
-          登出
-        </Button>
-      </div>
+  return (
+    <div className="m-4">
+      <Descriptions
+        items={items}
+        colon={false}
+        layout="vertical"
+        contentStyle={{ fontSize: '16px' }}
+      />
     </div>
   )
 }
